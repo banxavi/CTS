382b77cb3dd882f901ef73748a1b8d70ddacafac
Nhat <banxavi@gmail.com>
2021-03-31 11:22:02 +0700

    Nhat

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 from flask import Flask
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder='templates', static_folder='static/css')
 from app import admin
