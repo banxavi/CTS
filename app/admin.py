5adf3705937dce91e80ef153154fac918dfc0a1f
unknown <holongk15@gmail.com>
2021-03-31 14:57:40 +0700

    long

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from flask.templating import render_template
 from flask import Flask, render_template, redirect,url_for,request,flash,session,sessions
-from app import app
+# from app import app
 from flask_mysqldb import MySQL 
 import MySQLdb.cursors 
 import pymysql
@@ -8,7 +8,7 @@ import re
 from pymysql import cursors
 from werkzeug.utils import format_string
 
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # app.config['MYSQL_HOST'] = 'localhost'
 # app.config['MYSQL_USER'] = 'root'
@@ -19,14 +19,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Function LOGOUT
 @app.route('/')
-def home():
-    return render_template("home.html")
-
-@app.route('/logout')
-def logout():
+def login():
     return render_template("login.html")
-
-# Đổi thưởng user
-@app.route('/doithuonguser')
-def doithuonguser():
-    return render_template('exchange.html')
+if __name__=='__main__':
+    app.run(debug=True)
